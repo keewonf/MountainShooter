c9765e6ca58875d6424c48b3b3625677ddaca112
keewonf <lukasfmm@gmail.com>
2026-06-16 17:07:47 -0300

    feat: create menu interations, abc class, factory, bg parallax

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,31 @@
 # C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255,255,255)
+COLOR_YELLOW = (255, 255, 128)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
+}
 
 # M 
 MENU_OPTION = ('NEW GAME 1P',
